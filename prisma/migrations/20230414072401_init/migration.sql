8132f7e058a201a99e3cd325310172408e37215d
Nguyen Van Cuong <cuongnv@dipro.vn>
2023-04-14 14:24:26 +0700

    fix: fix backend

--- prisma/migrations/20230414072401_init/migration.sql
+++ prisma/migrations/20230414072401_init/migration.sql
@@ -17,7 +17,7 @@ CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
-    "role" "Role" NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'USER',
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
